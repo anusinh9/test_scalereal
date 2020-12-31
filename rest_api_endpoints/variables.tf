fb1fef486da05bf2ee7c5711a1b96fb93553468d
anusinh9 <anubhavsinha.anu@gmail.com>
2020-12-31 11:30:16 +0530

    Adding restapi code in separate folder

---
+++ rest_api_endpoints/variables.tf
@@ -0,0 +1,10 @@
+  
+variable "region" {
+  type        = string
+  description = "Region for all your resources"
+}
+
+variable "function_name" {
+  type        = string
+  description = "Name of your lambda function"
+}
